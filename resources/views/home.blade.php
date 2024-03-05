087a7023682afd308b113bd8192706155c9c1608
Aditya Restu <adityarestuhudayana@gmail.com>
2024-03-05 15:40:38 +0700

    menambahkan file test

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('template.main')
+
+@section('container')
+    <div class="px-4 py-5 text-center">
+        <img class="d-block mx-auto mb-4" src="img/shopping.png" alt="" width="300" height="200">
+        <h1 class="display-5 fw-bold text-body-emphasis">Shopping Everywhere</h1>
+        <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. At totam deserunt vitae voluptate
+                quo, adipisci sit, inventore assumenda odio obcaecati quam velit aliquid blanditiis tempora modi provident
+                similique natus debitis.</p>
+            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Explore</button>
+            </div>
+        </div>
+    </div>
+@endsection
