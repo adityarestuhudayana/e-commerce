d51b7f9d5afde6180d547ca181b6585caede00a2
Aditya Restu <adityarestuhudayana@gmail.com>
2024-03-06 16:58:25 +0700

    membuat database dan tabel product beserta tampilan nya

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,60 +2,35 @@
 
 @section('container')
     <select class="form-select mb-3" aria-label="Default select example">
-        <option selected>Open this select menu</option>
+        <option selected>Pilih kategori</option>
         <option value="1">One</option>
         <option value="2">Two</option>
         <option value="3">Three</option>
     </select>
 
     <div class="row">
-        <div class="col-md-3">
-            <div class="card">
-                <img src="img/shopping.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's
-                        content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+        @foreach ($products as $product)
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="img/shopping.png" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $product->nama_produk }}</h5>
+                        <hr>
+                        <p class="card-text">Keterangan : <br><small>{{ $product->exerpt }}</small></p>
+
+                        <div class="row">
+                            <button type="button" class="btn btn-danger position-relative mb-2">
+                                Rp {{ number_format($product->harga) }} <i class="bi bi-bag-check-fill"></i>
+                                <span
+                                    class="position-absolute top-0 start-100 translate-middle p-2 bg-success border border-light rounded-circle">
+                                    <i class="bi bi-star-fill"></i>
+                                </span>
+                            </button>
+                            <a href="#" class="btn btn-primary">Pesan sekarang</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img src="img/shopping.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's
-                        content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img src="img/shopping.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's
-                        content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <img src="img/shopping.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's
-                        content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
+        @endforeach
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/products', function(){
-    return view('products.index');
-});
+Route::resource('/products', ProductController::class);
