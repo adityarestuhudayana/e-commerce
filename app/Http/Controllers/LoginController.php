f6a35e116bac8d047ecfefce125cf64ecf17b4f3
Aditya Restu <adityarestuhudayana@gmail.com>
2024-03-09 11:33:55 +0700

    membuat product di profile

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
@@ -61,6 +62,8 @@ class LoginController extends Controller
 
     public function profile()
     {
-        return view('profile.index');
+        return view('profile.index', [
+            'products' => Product::latest()->get()
+        ]);
     }
 }
